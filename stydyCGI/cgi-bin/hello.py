79571e28d410b0e5b1c915bf022235532894f331
ttom921 <ttom921@hotmail.com>
2023-10-13 09:31:33 +0800

    feat: 加入CGI測試

---
+++ stydyCGI/cgi-bin/hello.py
@@ -0,0 +1,12 @@
+print("Content-Type: text/html\n\n")
+
+print("<!doctype html>")
+print("<head>")
+print("<title>hello world</title>")
+print("<meta charset='utf-8'")
+print("</head>")
+print("<body>")
+print("<h2>hello world CGI!!</h2>")
+print("</body>")
+print("</html")
+# python.exe -m http.server --bind localhost --cgi 8000
