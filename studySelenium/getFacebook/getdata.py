0e844300e6f0f7e4bcc0cb63a83eb627712cc3be
ttom921 <ttom921@hotmail.com>
2023-10-13 09:30:23 +0800

    update: 加入小說

--- studySelenium/getFacebook/getdata.py
+++ studySelenium/getFacebook/getdata.py
@@ -6,20 +6,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 #
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+# import Action chains
+from selenium.webdriver.common.action_chains import ActionChains
 
+import requests as rq
 from bs4 import BeautifulSoup
 
 
 @contextmanager
 def make_chromw_driver() -> Chrome:
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--start-maximized")
+    # options.add_argument("--headless")
+    # options.add_argument("--start-maximized")
+    options.add_argument("--auto-open-devtools-for-tabs")
     options.add_argument(f"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                          f"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36")
-
+    # options.add_argument(f"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
     driver = Chrome(ChromeDriverManager().install(), options=options)
     yield driver
     driver.quit()
@@ -27,14 +32,27 @@ def make_chromw_driver() -> Chrome:
 
 def demo_facebook_data():
     with make_chromw_driver() as driver:
-        driver.get("https://www.facebook.com/profile.php?id=100078089262603")
+        driver.get("https://m.facebook.com/people/%E6%AF%8F%E6%97%A5%E7%82%BA%E5%AD%A9%E5%AD%90%E7%A6%B1%E5%91%8A/100078089262603/")
         time.sleep(5)
+        # perform the operation
+        # actions = ActionChains(driver)
+        # actions.send_keys(Keys.LEFT_CONTROL, Keys.SHIFT, "M")
+        # actions.perform()
+
+        ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('m').perform()
+
+        time.sleep(5)
+
+        # driver.get("https://m.facebook.com/people/%E6%AF%8F%E6%97%A5%E7%82%BA%E5%AD%A9%E5%AD%90%E7%A6%B1%E5%91%8A/100078089262603/")
+        # time.sleep(5)
         soup = BeautifulSoup(driver.page_source, "html.parser")
         # 取得要如何拿到class
         # print(soup.prettify())
-        # results = soup.find_all("div", dir="auto")
-        all_results = soup.find_all("div", {'style': 'text-align: start;'})
-        # print(all_results)
+        results = soup.find_all("div", dir="auto")
+        print(results)
+        all_results = soup.find_all("div", class_="native-text")
+        print(all_results)
+        return
 
         idx = 0
         for content in all_results:
@@ -55,5 +73,16 @@ def demo_facebook_data():
         #     print(result.text)
 
 
+def getPageData(url):
+    # print(url)
+    response = rq.get(url)  # 用 requests 的 get 方法把網頁抓下來
+    html_doc = response.text  # text 屬性就是 html 檔案
+    soup = BeautifulSoup(response.text, "html.parser")
+    results = soup.find_all("div", dir="auto")
+    print(results)
+
+
 if __name__ == '__main__':
+    # url = "https://www.facebook.com/people/%E6%AF%8F%E6%97%A5%E7%82%BA%E5%AD%A9%E5%AD%90%E7%A6%B1%E5%91%8A/100078089262603/"
+    # getPageData(url)
     demo_facebook_data()
--- studySelenium/getNovelOne/getdata.py
+++ studySelenium/getNovelOne/getdata.py
@@ -78,8 +78,8 @@ def getResultData(reslst):
         except:
             print('An exception occurred')
     # print("resultdic=", resultdic)
-    # for key, value in sorted(resultdic.items()):
-    #     print("{} = {}".format(key, value))
+    for key, value in resultdic.items():
+        print("{} = {}".format(key, value))
     result = sorted(resultdic.items())
     return result
 
@@ -97,22 +97,70 @@ def replaceText(rowtext):
     return string
 
 
+def addNewLine(text):
+    line_length = 60
+    lines = []
+    for i in range(0, len(text), line_length):
+        lines.append(text[i:i + line_length] + '\n')
+    result = ""
+    for x in lines:
+        result += x + CRLF
+    return result
+
+
+def addNormal(text):
+    return text
+
+
 def getPageData(url):
     # print(url)
+    textstr = ""
     response = rq.get(url)  # 用 requests 的 get 方法把網頁抓下來
     html_doc = response.text  # text 屬性就是 html 檔案
-    bpage = BeautifulSoup(response.text, "lxml")
-    # print(bpage.prettify())
-    pagedata = bpage.find("div", {"id": "contentbox"})
+    bpage = BeautifulSoup(response.text, "html.parser")
+    divcontent = bpage.find("div", {"id": "contentbox"})
+
+    # print(divcontent.prettify())
+    # 檢查長度
+    temptext = divcontent.getText("\n\n").strip()
+    # print(len(temptext))
+    if(len(temptext) > 0):
+        textstr = temptext
+    else:
+        pass
+
+        pagedata = divcontent.find("p")
+
+        if pagedata is not None:
+            # print("000000000000000000000")
+            # print(pagedata.prettify())
+            textstr = pagedata.getText("\n\n")
+        else:
+            textstr = divcontent.find("div", {"id": "contentbox"}).getText("\n\n")
+            # print("***************************")
+            # print(textstr)
+    print(simplified2Traditional(replaceText(textstr)))
+    # if pagedata is not None:
+    #     print(simplified2Traditional(replaceText(pagedata)))
+    # else:
+    #     pagedata = bpage.find("div", {"id": "contentbox"}).getText("\n")
+    #     print(simplified2Traditional(replaceText(pagedata)))
+    return
+    # pagedata = bpage.find("div", {"id": "contentbox"})
+    # print(pagedata)
+    return
     # print("-----------------------------")
     rowtext = ""
     for item in pagedata:
+        print(item.text)
         if isIgnorChar(item) == False:
             # print(item.text)
-            rowtext += item.text + CRLF
+            rowtext += addNewLine(item.text)
+            # rowtext += addNormal(item.text)
     # print(repr(rowtext))
     # print("-----------------------------")
-    rowtext = simplified2Traditional(replaceText(rowtext))
+    # rowtext = simplified2Traditional(replaceText(rowtext))
+    rowtext = simplified2Traditional(rowtext)
     # print(rowtext)
     return rowtext
 
@@ -145,29 +193,29 @@ def writeToFile(title, content):
         f.write(content)
 
 
-def getOneChapData(idx, resultlst):
-    # 56287.html 第一章
-    url = "https://tw.uukanshu.com"
-    # print(type(resultlst))
+# def getOneChapData(url, idx, resultlst):
+#     # 56287.html 第一章
+#     # url = "https://tw.uukanshu.com"
+#     # print(type(resultlst))
 
-    curridx = idx - 1
-    # print(f'curridx={curridx}')
-    dicdata = resultlst[curridx]
-    datapack = dicdata[1]
-    # print(datapack)
-    rowdata = datapack["rowdata"]
-    htmdata = datapack["html"]
-    # print(f'rowdata={rowdata}')
-    # print(f'htmdata={htmdata}')
-    lhref = rowdata["href"]
-    # print(lhref)
-    eurl = url + lhref
-    print(eurl)
-    title = rowdata["title"]
-    ftitle = '{0:04}'.format(curridx) + '-' + title
-    print(ftitle)
-    contenttxt = getPageData(eurl)
-    print(contenttxt)
+#     curridx = idx - 1
+#     # print(f'curridx={curridx}')
+#     dicdata = resultlst[curridx]
+#     datapack = dicdata[1]
+#     # print(datapack)
+#     rowdata = datapack["rowdata"]
+#     htmdata = datapack["html"]
+#     # print(f'rowdata={rowdata}')
+#     # print(f'htmdata={htmdata}')
+#     lhref = rowdata["href"]
+#     # print(lhref)
+#     eurl = url + lhref
+#     print(eurl)
+#     title = rowdata["title"]
+#     ftitle = '{0:04}'.format(curridx) + '-' + title
+#     print(ftitle)
+#     contenttxt = getPageData(eurl)
+#     print(contenttxt)
 
 
 def getPageHTMLdata(eurl):
@@ -177,113 +225,174 @@ def getPageHTMLdata(eurl):
     print(soup.prettify())  # 把排版後的 html 印出來
 
 
-def demo_get_novel2():
-    mid = '33'
+def getChapListData(bschapList):
+    lst = []
+    for item in bschapList:
+        adata = item.find("a", href=True)
+        # print(adata)
+        if adata is not None:
+            href = adata['href']
+            title = adata['title']
+            data = {
+                "href": href,
+                "title": title
+            }
+            lst.insert(0, data)
+            # print(data)
+    # print(lst)
+    return lst
+
+
+def getOneChapData(url, idx, lst):
+    # 56287.html 第一章
+    # url = "https://tw.uukanshu.com"
+    # print(type(resultlst))
+
+    curridx = idx - 1
+    # print(f'curridx={curridx}')
+    dicdata = lst[curridx]
+    # print(dicdata)
+    # datapack = dicdata[1]
+    # # print(datapack)
+    # rowdata = datapack["rowdata"]
+    # htmdata = datapack["html"]
+    # # print(f'rowdata={rowdata}')
+    # # print(f'htmdata={htmdata}')
+    lhref = dicdata["href"]
+    # print(lhref)
+    ltitle = dicdata["title"]
+    msg = f"{curridx}---{ltitle}"
+    print(msg)
+    # print(f"{lhref}--{ltitle}")
+    eurl = url + lhref
+    # print(eurl)
+    contenttxt = getPageData(eurl)
+    print(contenttxt)
+
+
+def demo_get_twuuweb():
+    #mid = '33'
+    mid = '76656'
     #print(f'word = {word}')
-    url = f"https://tw.uukanshu.com/b/{mid}/"  # PTT NBA 板
-    response = rq.get(url)  # 用 requests 的 get 方法把網頁抓下來
+    # https://t.uukanshu.com/book.aspx?id=5199
+    url = f"https://tw.uukanshu.com"
+    url_b = f"{url}/b/{mid}/"  # PTT NBA 板
+    response = rq.get(url_b)  # 用 requests 的 get 方法把網頁抓下來
     html_doc = response.text  # text 屬性就是 html 檔案
     soup = BeautifulSoup(response.text, "html.parser")  # 指定 lxml 作為解析器
     # print(soup.prettify())  # 把排版後的 html 印出來
-    lst = soup.find("ul", attrs={"id": "chapterList"}).find_all("li")
+    bschapList = soup.find("ul", attrs={"id": "chapterList"}).find_all("li")
+    # cls()
+    lst = getChapListData(bschapList)
     # print(lst)
-    resultlst = getResultData(lst)
-    chap = 2354
-    cls()  # resultlst = getResultData(lst)
-    # print(resultlst)   # print(resultlst)
-    print("*****************************")
-    print("*****************************")
-    print("*****************************")
-    getOneChapData(chap, resultlst)
-    print("-----------------------------")
-    print("-----------------------------")
-    print("-----------------------------")
-    return
-
-    chap = 2
-
-
-def demo_get_novel():
-    mid = '33939'
-    #print(f'word = {word}')
-    url = f"https://tw.uukanshu.com/b/{mid}/"  # PTT NBA 板
-    response = rq.get(url)  # 用 requests 的 get 方法把網頁抓下來
-    html_doc = response.text  # text 屬性就是 html 檔案
-    soup = BeautifulSoup(response.text, "html.parser")  # 指定 lxml 作為解析器
-    # print(soup.prettify())  # 把排版後的 html 印出來
-    # return
-    # <ul id="chapterList">
-    lst = soup.find("ul", attrs={"id": "chapterList"}).find_all("li")
-    # print(lst)
-# 56287.html 第一章
-# 56288 第二章
-# 56462.html 第三章
-
-    resultlst = getResultData(lst)
-    # print(resultlst)
-    # return
-    chap = 1758
+    chap = 4624
     cls()
     print("*****************************")
     print("*****************************")
-    print("*****************************")
-    getOneChapData(chap, resultlst)
+
+    getOneChapData(url, chap, lst)
     print("-----------------------------")
     print("-----------------------------")
-    print("-----------------------------")
+
     return
-    formatResultData(resultdic)
-    return
-    adata = lst[-2].find("a")
-    # print(adata['href'])
-    url = "https://tw.uukanshu.com"
-    eurl = url + adata['href']
-    print(eurl)
+# ===========================================================
+
+
+def getuuPageData(eurl):
+    # print(url)
+    textstr = ""
     response = rq.get(eurl)  # 用 requests 的 get 方法把網頁抓下來
     html_doc = response.text  # text 屬性就是 html 檔案
-    # bpage = BeautifulSoup(response.text, "lxml")  # 指定 lxml 作為解析器
-
-    # dct = {"c": 1, "b": 3, "a": 7}
-    # for key, value in sorted(dct.items()):
-    #     print("{} = {}".format(key, value))
-
     bpage = BeautifulSoup(response.text, "html.parser")
-    # print(bpage.prettify())
-    pagedata = bpage.find("div", {"id": "contentbox"})
-    starinfo = "=" * 40
-    print(starinfo)
-    # print(pagedata)
-    for item in pagedata:
-        if isIgnorChar(item) == False:
-            print(item.text)
-        # print(item.class.__name__)
-        # print(type(item))
-        # print(repr(item.text))
-        # if '\n' in item.text or '\r' in item.text or '\n\n\n\n' in item.text:
-        #     print("has n r ")
-        #     # print(type(item))
-        # if CRLF in item.text:
-        #     print("has crlf")
-        #     # print(type(item))
-        # print(repr(item.text))
-        # if(item.text != "") or (item.text != '\n') or (item.text != CRLF):
-        #     print(item.text)
-        # print(type(item).__name__)
-    #     if type(item).__name__ == "NavigableString":
-    #         # print(item)
-    #         print(item.text)
+    # print(bpage.prettify())  # 把排版後的 html 印出來
+    pcontent = bpage.find("p", {"class": "readcotent"})
+    # print(pcontent.prettify())
+    textstr = pcontent.getText()
+    print(textstr)
+    # print(simplified2Traditional(textstr))
+    print("\n")
 
-    #     # bs4.element.Tag
-    #     # print("_class is a instance of MyClass() : ", isinstance(item,bs4.element.Tag))
-    #     # addata = item.find("div", attrs={"class": "ad_content"})
-    #     # print(addata)
-    #     # if addata != None:
-    #     #     print(item)
-    # # for item in lst:
-    # #     print(item)
-    # # print(item.text)
+
+def getuuneChapData(url, idx, lst):
+    curridx = idx - 1
+    # print(f'curridx={curridx}')
+    dicdata = lst[curridx]
+    # print(dicdata)
+    # datapack = dicdata[1]
+    lhref = dicdata["href"]
+    # print(lhref)
+    ltitle = dicdata["title"]
+    msg = f"{curridx}---{ltitle}"
+    print(msg)
+    # print(f"{lhref}--{ltitle}")
+    eurl = url + lhref
+    # print(eurl)
+    getuuPageData(eurl)
+
+
+def getuuChapList(bschapList):
+    lst = []
+    for item in bschapList:
+        adata = item.find("a", href=True)
+        if adata is not None:
+            # print(adata)
+            # print(adata.text)
+            title = adata.text
+            href = adata['href']
+            data = {
+                "href": href,
+                "title": title
+            }
+            lst.append(data)
+    # print(lst)
+    return lst
+
+
+def demo_get_uuweb():
+    # https://uukanshu.cc/book/6750/
+
+    # ------
+    mid = '5600'
+    chap = 1750
+    # ------
+    # ------
+    # mid = '5316'
+    # chap = 44
+    # ------
+    url = f"https://uukanshu.cc"
+    url_b = f"{url}/book/{mid}/"
+    response = rq.get(url_b)  # 用 requests 的 get 方法把網頁抓下來
+    html_doc = response.text  # text 屬性就是 html 檔案
+    soup = BeautifulSoup(response.text, "html.parser")  # 指定 lxml 作為解析器
+    # print(soup.prettify())  # 把排版後的 html 印出來
+    cls()
+    bschapList = soup.find("div", attrs={"id": "list-chapterAll"}).find_all("dd")
+    # print(bschapList)
+    lst = getuuChapList(bschapList)
+    # print(lst)
+    # chap = 44
+
+    print("*****************************")
+    print("*****************************")
+    getuuneChapData(url, chap, lst)
+    print("-----------------------------")
+    print("-----------------------------")
+
+
+def demo_get_mtwuuweb():
+    mid = '5199'
+    #print(f'word = {word}')
+    # https://t.uukanshu.com/book.aspx?id=5199
+    url = f"https://t.uukanshu.com/book.aspx?id={mid}"
+    response = rq.get(url)  # 用 requests 的 get 方法把網頁抓下來
+    html_doc = response.text  # text 屬性就是 html 檔案
+    soup = BeautifulSoup(response.text, "html.parser")  # 指定 lxml 作為解析器
+    # print(soup.prettify())  # 把排版後的 html 印出來
+    lst = soup.find("ul", attrs={"id": "chapterList"}).find_all("li")
+    print(lst)
 
 
 if __name__ == '__main__':
-    # demo_get_novel()
-    demo_get_novel2()
+    demo_get_uuweb()
+    # demo_get_twuuweb()
+    # demo_get_mtwuuweb()
